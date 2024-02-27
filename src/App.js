5a852ec89858a9107e8b7c93cccc4a85eb0de840
Eric Berglund <ericberglund117@gmail.com>
2024-02-27 16:12:55 -0700

    Built out nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
   <div>
-   <h1 className='text-2xl font-bold'>Eric Berglund</h1>
+   <Navbar />
   </div>
   );
 }
