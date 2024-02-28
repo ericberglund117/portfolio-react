7d0dbd0230ce2b348e7187090a38ec7808ac8ee1
Eric Berglund <ericberglund117@gmail.com>
2024-02-27 21:32:47 -0700

    Create skills section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import './App.css';
+import About from './components/About';
 import Home from './components/Home';
 import Navbar from "./components/Navbar"
+import Skills from './components/Skills';
 
 function App() {
   return (
   <div>
    <Navbar />
    <Home />
+   <About />
+   <Skills />
   </div>
   );
 }
