a60648e7a4d02f9c41e6eabf1f851b9b098e14d2
Eric Berglund <ericberglund117@gmail.com>
2024-02-27 21:32:14 -0700

    Complete home section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,22 @@ import {HiArrowNarrowRight} from  'react-icons/hi'
 
 export const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#01161e]'>
+    <div name='home' className='w-full h-screen bg-[#01161e]'>
+        {/* container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-[#EB5F28]'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#EFF6E0]'>Eric Berglund</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#AEC3B0]'>I'm a Frontend Software Engineer</h2>
+            <p className='text-[#EFF6E0] py-4 max-w-[700px]'>Specializing in building and designing a user-focused digital experience. Currently, I'm working on 
+                builing responsive web applications and utilizing backend technologies to become a Full-Stack developer.</p>
+            <div>
+                <button className='text-[#EFF6E0] group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#EB5F28] hover:border-[#EB5F28]'>View Work 
+                    <span className='group-hover:rotate-90 duration-300'>
+                        <HiArrowNarrowRight className='ml-3' /> 
+                    </span>
+                </button>
+            </div>
+        </div>
 
     </div>
   )
