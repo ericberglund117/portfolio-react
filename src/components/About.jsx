7af3ec9050bef2657437de5cdc6f64954d8875d5
Eric Berglund <ericberglund117@gmail.com>
2024-02-28 21:34:36 -0700

    Switch about headline

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,19 +6,19 @@ const About = () => {
     <div name='about' className='w-full h-screen bg-[#AEC3B0] text-[#01161e]'>
       <div className='flex flex-col justify-center items-center w-full h-full'>
         <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className='sm:text-right pb-8 pl-4'>
-            <p className='text-4xl font-bold inline border-b-4 border-[#EB5F28]'>
-              About
-            </p>
-          </div>
+            <div className='sm:text-right text-4xl font-bold'>
+              <p>Hi, this is what I look like.</p>
+            </div>
           <div>
             <img className='w-50 mx-auto pb-4 pr-4' src={Headshot} alt="Headshot" />
           </div>
           </div>
           <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
-            <div className='sm:text-right text-4xl font-bold'>
-              <p>Hi, I'm Eric! Please take a look around.</p>
-            </div>
+          <div className='sm:text-right pb-8 pl-4'>
+            <p className='text-4xl font-bold inline border-b-4 border-[#EB5F28]'>
+              About
+            </p>
+          </div>
             <div>
               <p>I am a proactive, detail-oriented Frontend Engineer with a background in
                 education and software development. I have experience in Quality Assurance Automation for B2B SaaS applications
